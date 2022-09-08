7842cf9ebfb824e78323c033ea4aec69adedbe93
dadolyner <skulj.david@gmail.com>
2022-09-08 10:54:22 +0200

    custom poppins typeography

---
+++ src/components/Typeography/typeography.styled.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import './typeography.css';
+
+export const Header1 = styled.h1`
+	font-family: PoppinsSemiBold;
+	font-size: 64px;
+    margin: 0;
+`;
+export const Header2 = styled.h2`
+	font-family: PoppinsMedium;
+	font-size: 52px;
+    margin: 0;
+`;
+export const Header3 = styled.h3`
+	font-family: PoppinsMedium;
+	font-size: 40px;
+    margin: 0;
+`;
+export const Header4 = styled.h4`
+	font-family: PoppinsMedium;
+	font-size: 28px;
+    margin: 0;
+`;
+export const Header5 = styled.h5`
+	font-family: Poppins;
+	font-size: 20px;
+    margin: 0;
+`;
+export const Header6 = styled.h6`
+	font-family: Poppins;
+	font-size: 16px;
+    margin: 0;
+`;
+
+export const Paragraph = styled.p`
+    font-family: Poppins;
+	font-size: 14px;
+    margin: 0;
+`;
+
+export const Bold = styled.p`
+    font-family: PoppinsMedium;
+	font-size: 14px;
+    margin: 0;
+`;
+
+export const Italic = styled.p`
+    font-family: PoppinsItalic;
+	font-size: 14px;
+    margin: 0;
+`;
+
+export const Underline = styled.p`
+    font-family: Poppins;
+	font-size: 14px;
+    margin: 0;
+    text-decoration: underline;
+`;
+
